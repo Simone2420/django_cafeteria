b660edd7fbb80dd06cc76a5573076967a92d9d2f
Simone2420 <blasssimom@gmail.com>
2025-06-23 18:11:55 -0500

    date from update and created

--- products/models.py
+++ products/models.py
@@ -7,6 +7,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
     description = models.TextField(verbose_name="Descripcion")
     available = models.BooleanField(default=True, verbose_name="Disponible")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Actualizado")
     image = models.ImageField(upload_to='logo', null=True, blank=True, verbose_name="Imagen")
 
     def __str__(self):
