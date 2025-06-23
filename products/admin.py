82794c3514cdecf3741b3475a6392828af9e0d70
Simone2420 <blasssimom@gmail.com>
2025-06-23 17:56:56 -0500

    images

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ["name", "description", "price"]
+    search_fields = ["name", "description"]
+
+admin.site.register(Product, ProductAdmin)
