fbefef0f938bcdb855acd7bb824270758193414b
Simone2420 <blasssimom@gmail.com>
2025-06-25 11:14:06 -0500

    implementando vista de no login

--- orders/views.py
+++ orders/views.py
@@ -7,4 +7,7 @@ class MyOrderView(DetailView):
     template_name = "orders/my_order.html"
     context_object_name = "order"
     def get_object(self, queryset=None):
-        return Order.objects.filter(is_active=True).first()
+        if self.request.user.is_authenticated:
+            return Order.objects.filter(user=self.request.user, is_active=True).first()
+        else:
+            return None
